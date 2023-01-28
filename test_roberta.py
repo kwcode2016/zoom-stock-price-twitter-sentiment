87b4114018fac5a27980c60ffcd85d0027bac09f
Ken Wee <kwhome2002@gmail.com>
2023-01-28 03:44:27 -0800

    adding test_roberta.py

---
+++ test_roberta.py
@@ -0,0 +1,77 @@
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import numpy as np
+from scipy.special import softmax
+import csv
+import urllib.request
+
+# Preprocess text (username and link placeholders)
+def preprocess(text):
+    new_text = []
+ 
+ 
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+# Tasks:
+# emoji, emotion, hate, irony, offensive, sentiment
+# stance/abortion, stance/atheism, stance/climate, stance/feminist, stance/hillary
+
+task='sentiment'
+MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+
+print(f"\n\nMODEL string: {MODEL}\n\n")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+# download label mapping
+labels=[]
+mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+with urllib.request.urlopen(mapping_link) as f:
+    html = f.read().decode('utf-8').split("\n")
+    csvreader = csv.reader(html, delimiter='\t')
+labels = [row[1] for row in csvreader if len(row) > 1]
+
+# PT
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+# model.save_pretrained(MODEL)
+
+# text = "Good night 😊"
+# text = "This is not so good"
+text = "The context of the date is not satisfactory"
+text = preprocess(text)
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+# # TF
+# model = TFAutoModelForSequenceClassification.from_pretrained(MODEL)
+# model.save_pretrained(MODEL)
+
+# text = "Good night 😊"
+# encoded_input = tokenizer(text, return_tensors='tf')
+# output = model(encoded_input)
+# scores = output[0][0].numpy()
+# scores = softmax(scores)
+
+
+print("testing...")
+
+
+ranking = np.argsort(scores)
+ranking = ranking[::-1]
+for i in range(scores.shape[0]):
+    l = labels[ranking[i]]
+    s = scores[ranking[i]]
+    print(f"{i+1}) {l} {np.round(float(s), 4)}")
+
+
+
+## Note: To fix the error message on first run.
+## https://stackoverflow.com/questions/69065131/oserror-when-loading-tokenizer-for-huggingface-model
+## It seems that this solution will remotely connect to huggingface. try to find how to use local model.
